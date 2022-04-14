785d5deaef84bb8dfd08285b24d802fade730574
John <nhgg6bkhjb@privaterelay.appleid.com>
2022-04-14 18:22:00 -0500

    Build round play functionality

--- script.js
+++ script.js
@@ -3,15 +3,39 @@ function computerPlay() {
   let randomNum = Math.floor(Math.random() * 3 + 1);
   switch (randomNum) {
     case 1:
-      item = "Rock";
+      item = "rock";
       break;
     case 2:
-      item = "Paper";
+      item = "paper";
       break;
     case 3:
-      item = "Scissors";
+      item = "scissors";
     default:
       break;
   }
   return item;
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerPlay();
+  if (playerSelection == "rock" && computerSelection == "rock") {
+    return "It's a tie!";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win! Rock beats scissors";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You lose! Paper beats rock";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You win! Paper beats rock";
+  } else if (playerSelection == "paper" && computerSelection == "paper") {
+    return "It's a tie!";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You lose! Scissors beat paper";
+  } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+    return "It's a tie!";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You win! Scissors beat paper";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You lose! Rock beats scissors";
+  }
 }
